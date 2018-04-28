2a8986256e7230976b846cb33a2d2cda0c39d3b3
ly609098 <ly609098@aliyun.com>
2018-04-29 03:27:03 +0800

    init message

---
+++ boot1/src/main/java/com/demo/boot1/Boot1Application.java
@@ -0,0 +1,12 @@
+package com.demo.boot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot1Application.class, args);
+    }
+}
